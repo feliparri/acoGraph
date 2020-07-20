20616a62d60d07dc3319aaf76b32762b1b1be5eb
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2020-07-20 17:26:33 -0400

    ADD: date between filter

--- app/Http/Controllers/ControllerReports.php
+++ app/Http/Controllers/ControllerReports.php
@@ -8,7 +8,15 @@ use Illuminate\Support\Facades\DB;
 class ControllerReports extends Controller
 {
     public function getResumenLote(Request $request){
-        $lote = \App\VResumenLote::orderBy($request->sortBy==null?'tipo_Mov':$request->sortBy, $request->descending==true?'desc':'asc')->paginate($request->rowsPerPage, ['*'], 'page', $request->page);
-        return response()->json($lote);
+        // return isset($request->filter['from']);
+        $lote = \App\VResumenLote::orderBy($request->sortBy==null?'tipo_Mov':$request->sortBy, $request->descending==true?'desc':'asc');
+        if(isset($request->filter['from'])){
+            $lote = $lote->where('fecha','>', $request->filter['from']);
+        }
+        if(isset($request->filter['to'])){
+            $lote = $lote->where('fecha','<', $request->filter['to']);
+        }
+
+        return response()->json($lote->paginate($request->rowsPerPage, ['*'], 'page', $request->page));
     }
 }
