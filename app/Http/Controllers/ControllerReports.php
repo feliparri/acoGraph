0271e394810f8ba5109c4d1312219ae661969119
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2020-07-22 17:15:10 -0400

    ADD:new querys for peso x mes and get data by tipo fruta

--- app/Http/Controllers/ControllerReports.php
+++ app/Http/Controllers/ControllerReports.php
@@ -19,4 +19,53 @@ class ControllerReports extends Controller
 
         return response()->json($lote->paginate($request->rowsPerPage, ['*'], 'page', $request->page));
     }
+
+    public function getPieChartData(Request $request){
+        // return isset($request->filter['from']);
+        $lote = \App\VResumenLote::groupBy('especie')
+        ->selectRaw('especie, sum(peso_neto) as sum')
+        ->get();
+
+        if(isset($request->filter['from'])){
+            $lote = $lote->where('fecha','>', $request->filter['from']);
+        }
+        if(isset($request->filter['to'])){
+            $lote = $lote->where('fecha','<', $request->filter['to']);
+        }
+
+        return response()->json($lote);
+    }
+
+    public function getPieChartDataByCodVariedad(Request $request){
+        // return isset($request->filter['from']);
+        $lote = \App\VResumenLote::groupBy('cod_variedad')
+        ->selectRaw('count(cod_variedad) as count, cod_variedad ')
+        ->get();
+
+        if(isset($request->filter['from'])){
+            $lote = $lote->where('fecha','>', $request->filter['from']);
+        }
+        if(isset($request->filter['to'])){
+            $lote = $lote->where('fecha','<', $request->filter['to']);
+        }
+
+        return response()->json($lote);
+    }
+
+    public function getPieChartDataByPesoMes(Request $request){
+        // return isset($request->filter['from']);
+        $lote = \App\VResumenLote::groupBy('mes')
+        ->selectRaw('sum(peso_neto) as peso, MONTH(fecha_cosecha) mes')
+        ->get();
+        #select sum(peso_neto), MONTH(fecha_cosecha) from v_resumen_lote vrl group by MONTH(fecha_cosecha) order by MONTH(fecha_cosecha) asc
+        if(isset($request->filter['from'])){
+            $lote = $lote->where('fecha','>', $request->filter['from']);
+        }
+        if(isset($request->filter['to'])){
+            $lote = $lote->where('fecha','<', $request->filter['to']);
+        }
+
+        return response()->json($lote);
+    }
+
 }
